122f7cac85ff97262693f6efba239f719f99831a
Marian Krotov <krotovmarik@gmail.com>
2022-09-20 00:31:39 +0300

    первая программа

--- HW1.py
+++ HW1.py
@@ -1,10 +1,6 @@
-a = 546
-b: str = 'Моя первая программа'
-c = (a , b)
-d = {'a': '546', 'b': 'Моя первая программа'}
-e = (a*234)
-print(a)
-print(b)
-print(c)
-print(d)
-print(b , e)
+var1 = 546
+var2 = 'Моя первая программа'
+var3 = [var1, var2]
+var4 = {'a': var1, 'b': var2}
+var5 = (" ".join([str(var1 * 234), var2]))
+print(var1, var2, var3, var4, var5, sep="\n")
