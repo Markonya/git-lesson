14eb44a0654a07a3b71bd3c635a3a0792c166831
Marian Krotov <krotovmarik@gmail.com>
2022-09-20 00:32:17 +0300

    первая программа

--- HW1.py
+++ HW1.py
@@ -3,4 +3,4 @@ var2 = 'Моя первая программа'
 var3 = [var1, var2]
 var4 = {'a': var1, 'b': var2}
 var5 = (" ".join([str(var1 * 234), var2]))
-print(var1, var2, var3, var4, var5, sep="\n")
+print(var1, var2, var3, var4, var5, sep="\n")s
