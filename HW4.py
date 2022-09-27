2a3ffc2815f476d58d53632911266ac086d696ff
Marian Krotov <krotovmarik@gmail.com>
2022-09-27 16:42:26 +0300

    HW4 сортировка не работает

--- HW4.py
+++ HW4.py
@@ -1,14 +1,23 @@
 count_lines = {}
-for line in open(r'C:\Games\Python\q4_urls.txt', encoding='utf8'):
-    clean_line = line.rstrip()
+new_count_lines = {}
+for line in open('q4_urls.txt', encoding='utf8'):
+    clean_line = line.strip()
     if clean_line not in count_lines:
         count_lines[clean_line] = 0
     count_lines[clean_line] += 1
 
-out = open(r'C:\Games\Python\q4_urls_result.txt', 'w', encoding='utf8')
-for line in open(r'C:\Games\Python\q4_urls.txt', encoding='utf8'):
-    count = count_lines[line.rstrip()]
-    out.write(f'{line.strip()}\t{count}\n')
+out = open('q4_urls_result.txt', 'w', encoding='utf8')
+for line in count_lines:
+    sorted_values = sorted(count_lines.values())
+    for i in sorted_values:
+
+        for k in count_lines.keys():
+            if count_lines[k] == i:
+                new_count_lines[k] = count_lines[k]
+            break
+
+count = new_count_lines[line.rstrip()]
+
+out.write(f"{line.strip()}\t{count}\n")
 
 out.close()
-
