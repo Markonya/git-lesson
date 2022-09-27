fd331fa7a593883e2c7cc238e9295639002cf73d
Marian Krotov <krotovmarik@gmail.com>
2022-09-27 18:00:43 +0300

    HW4 сортировка работает

--- HW4.py
+++ HW4.py
@@ -8,10 +8,9 @@ for line in open('q4_urls.txt', encoding='utf8'):
     count_lines[clean_line] += 1
 
 out = open('q4_urls_result.txt', 'w', encoding='utf8')
-for line in count_lines:
-    sorted_count_lines = dict(sorted(count_lines.items(), key=lambda item: item[1]))
-    count = sorted_count_lines[line.rstrip()]
-    # print(count)
-    out.write(f'{line.strip()}\t{count}\n')
-    # print(sorted_count_lines)
+for key in sorted(count_lines.keys()):
+    # sorted_count_lines = dict(sorted(count_lines.items(), key=lambda item: item[1]))
+    # count = sorted_count_lines[line.rstrip()]
+    out.write(f'{key.strip()}\t{count_lines[key]}\n')
+
 out.close()
