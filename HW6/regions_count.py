1fb42a1db425efb9308aa0f7d883866102db2199
Marian Krotov <krotovmarik@gmail.com>
2022-10-02 21:45:12 +0300

    HW6 works

---
+++ HW6/regions_count.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+def count_regions():
+    with sqlite3.connect("cities.db") as conn:
+        cursor = conn.cursor()
+        cursor.executemany("""SELECT 
+        "region.name" as region, 
+        COUNT("city.name") as city 
+        FROM database WHERE region.id = cities.id
+        GROUP BY
+        cities.id
+        """)
+        res = cursor.fetchall()
+        print(res)
+    conn.commit()
+    conn.close()
