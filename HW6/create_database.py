61c6c0d3550aa282c7772f0a05df984814279ac3
Marian Krotov <krotovmarik@gmail.com>
2022-10-02 19:25:10 +0300

    HW6 works

--- HW6/create_database.py
+++ HW6/create_database.py
@@ -6,7 +6,10 @@ cur = con.cursor()
 cur.execute("""CREATE TABLE database ('region.name' TEXT, 'city.name' TEXT);""")
 
 with open('towns.csv', 'r', encoding='utf8') as fin:
-    dr = csv.DictReader(fin)
+    dr = csv.DictReader(fin, delimiter=',')
+    # for line in dr:
+        # print(line['region_name'])
+        # print(line['city'])
     to_db = [(i['region_name'], i['city']) for i in dr]
 
 cur.executemany("""INSERT INTO database ('region.name', 'city.name') VALUES (?,?);""", to_db)
