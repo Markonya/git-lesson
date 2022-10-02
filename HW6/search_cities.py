c068a5d37e2e654eb9cb6fe343c82e2c8f8b40ec
Marian Krotov <krotovmarik@gmail.com>
2022-10-02 21:49:59 +0300

    HW6 works

--- HW6/search_cities.py
+++ HW6/search_cities.py
@@ -5,7 +5,12 @@ from sqlite3 import Cursor
 def search_cities():
     with sqlite3.connect("cities.db") as conn:
         cursor: Cursor = conn.cursor()
-        cursor.executemany("""SELECT "city.name" FROM database WHERE "region.name" MATCH 'Архангельская область'  """)
+        cursor.executemany("""
+        SELECT "city.name" 
+        FROM database 
+        WHERE "region.name" 
+        MATCH 'Архангельская область'  
+        """)
         res = cursor.fetchall()
     if res:
         print(res)
