1e147fd3ad17788df036d564127a91af52aad506
Marian Krotov <krotovmarik@gmail.com>
2022-10-03 22:06:09 +0300

    HW6 in progress

--- HW6/create_database.py
+++ HW6/create_database.py
@@ -3,13 +3,13 @@ import sqlite3
 
 con = sqlite3.connect("cities.db")
 cur = con.cursor()
-cur.execute("""CREATE TABLE region (id INT, 'region.name' TEXT);""")
-# как пронумеровать id автоматически? как удалить повторяющиеся регионы?
-cur.execute("""CREATE TABLE city (id INT, 'city.name' TEXT, region_id TEXT,
+cur.execute("""CREATE TABLE region (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'region.name' TEXT);""")
+# как удалить повторяющиеся регионы?
+cur.execute("""CREATE TABLE city (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'city.name' TEXT, region_id TEXT,
 FOREIGN KEY (region_id) references region(id));""")
-# как пронумеровать id автоматически? Верно связь сделал?
+# Корректно ли так связь указывать?
 
-with open('towns.csv', 'r', encoding='utf8') as fin2:
+with open('towns.csv', 'r', encoding='utf8') as fin1:
     dr1 = csv.DictReader(fin1, delimiter=',')
     to_db1 = [(i['region_name'], ) for i in dr1]
 
