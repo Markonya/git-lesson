5925ad282311908695ab9d34f045de9b9c744797
Marian Krotov <krotovmarik@gmail.com>
2022-10-03 22:00:53 +0300

    HW6 in progress

--- HW6/create_database.py
+++ HW6/create_database.py
@@ -3,16 +3,22 @@ import sqlite3
 
 con = sqlite3.connect("cities.db")
 cur = con.cursor()
-cur.execute("""CREATE TABLE database ('region.name' TEXT, 'city.name' TEXT);""")
+cur.execute("""CREATE TABLE region (id INT, 'region.name' TEXT);""")
+# как пронумеровать id автоматически? как удалить повторяющиеся регионы?
+cur.execute("""CREATE TABLE city (id INT, 'city.name' TEXT, region_id TEXT,
+FOREIGN KEY (region_id) references region(id));""")
+# как пронумеровать id автоматически? Верно связь сделал?
 
-with open('towns.csv', 'r', encoding='utf8') as fin:
-    dr = csv.DictReader(fin, delimiter=',')
-    # for line in dr:
-        # print(line['region_name'])
-        # print(line['city'])
-    to_db = [(i['region_name'], i['city']) for i in dr]
+with open('towns.csv', 'r', encoding='utf8') as fin2:
+    dr1 = csv.DictReader(fin1, delimiter=',')
+    to_db1 = [(i['region_name'], ) for i in dr1]
 
-cur.executemany("""INSERT INTO database ('region.name', 'city.name') VALUES (?,?);""", to_db)
+with open('towns.csv', 'r', encoding='utf8') as fin2:
+    dr2 = csv.DictReader(fin2, delimiter=',')
+    to_db2 = [(i['city'], ) for i in dr2]
+
+cur.executemany("""INSERT INTO region ('region.name') VALUES (?);""", to_db1)
+cur.executemany("""INSERT INTO city ('city.name') VALUES (?);""", to_db2)
 con.commit()
 con.close()
 
