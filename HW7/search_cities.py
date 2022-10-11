05de9c562ff8ad5ec2cd305def123516c2384aec
Marian Krotov <krotovmarik@gmail.com>
2022-10-11 23:45:30 +0300

    HW7 finished

--- HW7/search_cities.py
+++ HW7/search_cities.py
@@ -8,6 +8,10 @@ search_key = 'Архангельская область'
 search_region_id = regions[search_key]
 with sqlite3.connect("cities.db") as connection:
     cursor = connection.cursor()
-    result = cursor.execute("""SELECT name FROM city WHERE region_id={0};""".format(search_region_id))
+    result = cursor.execute("""
+        SELECT city.name, city.region_id 
+        FROM city, region 
+        WHERE city.region_id = region.id AND region.name LIKE '{0}';
+        """.format(search_key))
     print(*sorted([city[0] for city in result.fetchall()]), sep='\n')
 
