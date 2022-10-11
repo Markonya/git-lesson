80b9640b9ac6b1ba5712b370fb63d5b6394bab64
Marian Krotov <krotovmarik@gmail.com>
2022-10-11 23:47:07 +0300

    HW7 finished

--- HW7/search_cities.py
+++ HW7/search_cities.py
@@ -4,8 +4,6 @@ from create_database import regions
 # search_key = input('Введите регион: ')
 search_key = 'Архангельская область'
 
-
-search_region_id = regions[search_key]
 with sqlite3.connect("cities.db") as connection:
     cursor = connection.cursor()
     result = cursor.execute("""
